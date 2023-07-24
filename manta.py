d69ebc5c1ebedf32624d7c22fa148981b38d8ffa
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-07-24 12:10:13 -0700

    Update manta.py

--- manta.py
+++ manta.py
@@ -1,5 +1,6 @@
 import string,sys,os,glob
-  
+
+#Germline (https://github.com/Illumina/manta/blob/master/docs/userGuide/README.md)
 def STEP1_Configuration(REFERENCE_GENOME,bam_list,path):
     os.system('configManta.py '+' '.join(map(lambda z:'--bam '+path+z, bam_list))+' --referenceFasta '+REFERENCE_GENOME+' --runDir output_directory')
 
